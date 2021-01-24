dee8346e7b41babb04ec5dca22c4edcbeff5bdd1
dnikolova1 <nikolovadara@gmail.com>
2021-01-24 14:54:50 -0600

    final

--- noober.js
+++ noober.js
@@ -18,6 +18,7 @@ passenger1PickupAddressLine2 = `${ride[0].pickupLocation.city}, ${ride[0].pickup
 passenger1DropoffAddressLine1 = `${ride[0].dropoffLocation.address}`
 passenger1DropoffAddressLine2 = `${ride[0].dropoffLocation.city}, ${ride[0].pickupLocation.state} ${ride[0].pickupLocation.zip}`
 
+if (ride.length >1) {
 passenger2Name = `${ride[1].passengerDetails.first} ${ride[1].passengerDetails.last}`
 passenger2Phone = `${ride[1].passengerDetails.phoneNumber}`
 passenger2NumberOfPassengers = `${ride[1].numberOfPassengers}`
@@ -25,8 +26,9 @@ passenger2PickupAddressLine1 = `${ride[1].pickupLocation.address}`
 passenger2PickupAddressLine2 = `${ride[1].pickupLocation.city}, ${ride[1].pickupLocation.state} ${ride[1].pickupLocation.zip}`
 passenger2DropoffAddressLine1 = `${ride[1].dropoffLocation.address}`
 passenger2DropoffAddressLine2 = `${ride[1].dropoffLocation.city}, ${ride[1].pickupLocation.state} ${ride[1].pickupLocation.zip}`
+}
 
-
+if (ride.length >2) {
 passenger3Name = `${ride[2].passengerDetails.first} ${ride[2].passengerDetails.last}`
 passenger3Phone = `${ride[2].passengerDetails.phoneNumber}`
 passenger3NumberOfPassengers = `${ride[2].numberOfPassengers}`
@@ -34,7 +36,7 @@ passenger3PickupAddressLine1 = `${ride[2].pickupLocation.address}`
 passenger3PickupAddressLine2 = `${ride[2].pickupLocation.city}, ${ride[2].pickupLocation.state} ${ride[2].pickupLocation.zip}`
 passenger3DropoffAddressLine1 = `${ride[2].dropoffLocation.address}`
 passenger3DropoffAddressLine2 = `${ride[2].dropoffLocation.city}, ${ride[2].pickupLocation.state} ${ride[2].pickupLocation.zip}`
-
+}
 
   // 🔥🔥🔥 YOUR CODE GOES HERE 🔥🔥🔥
   // the 'ride' variable contains data about a random ride (i.e. when the page)
